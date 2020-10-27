e3016da43bae9f69f36d717abbebc1d021326b3d
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-27 09:27:58 +0800

    Added MultiCounter.js

---
+++ src/components/MultiCounter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import CounterGroup from './CounterGroup.js'
+import CounterSizeGenerator from './CounterSizeGenerator.js'
+import CounterGroupSum from './CounterGroupSum.js'
+
+class MultiCounter extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = { size: 0, sum: 0};
+    }
+    
+    onGenerate = (size) => {
+        this.setState({ size });
+    }
+
+    onSum = (value) => {
+        this.setState(() => ({sum: value}))
+    }
+
+    render() {
+        return (
+            <div>
+                <CounterSizeGenerator onGenerate={this.onGenerate}/>
+                <CounterGroupSum sum={this.state.sum}/>
+                <CounterGroup size={this.state.size} onSum={this.onSum}/>
+            </div>
+        );
+    }
+}
+
+export default MultiCounter;
