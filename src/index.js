03deec3927554c8ce5074f6f8cde3ecb79ba7b47
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-27 14:33:08 +0800

    Modified index.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render (
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
