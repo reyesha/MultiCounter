e1f5d9ce9fa2bb7f07d45fcb7718cf549020b332
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-27 10:08:21 +0800

    Added app.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MultiCounter from './components/MultiCounter'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MultiCounter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
